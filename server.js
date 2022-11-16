c22cee8ed2c5d4c51456de8d35ad13643289ebb3
briancerqueira <113534138+briancerqueira@users.noreply.github.com>
2022-11-16 16:15:10 -0300

    Arquivo atualizado

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router ('db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => {
-    //eslint-disable-next-line no console
-    console.log(`JSON Server está funcionando na porta ${port}`);
-});
+server.listen(port, ()=> {console.log(`JSON Server está rodando na porta ${port}`)})
+console.log('TESTE')
