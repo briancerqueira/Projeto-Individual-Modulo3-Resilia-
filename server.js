188c2247b07fb5b87e246018380529abeb63ec80
briancerqueira <113534138+briancerqueira@users.noreply.github.com>
2022-11-21 13:49:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(jsonServer.rewriter({
-  "/todos_os_funcionarios": "/funcionarios",
-  "/adicionar_funcionario": "/funcionarios",
-  "/editar_funcionario/:id": "/funcionarios/:id",
-  "/deletar_funcionario/:id": "/funcionarios/:id",
-  "/teste": "/funcionarios"
+  "/todos_os_empregados": "/empregados",
+  "/adicionar_empregado": "/empregados",
+  "/editar_empregado/:id": "/empregados/:id",
+  "/deletar_empregado/:id": "/empregados/:id"
 }))
 server.use(router);
 server.listen(port, () => { console.log(`JSON Server está rodando na porta ${port}`) })
